c6903c1a0fd1b0346036aaf29155e7f4cd24ef56
yacine djaaraoui <djaraoui60@gmail.com>
2024-08-17 14:33:10 +0200

    remove magnify

--- frontend/src/comp/ProductImages.tsx
+++ frontend/src/comp/ProductImages.tsx
@@ -9,20 +9,19 @@ import {
 } from "../components/ui/carousel";
 import { useState, useEffect, useCallback } from "react";
 import { ShopContext } from "./ShopContext";
-import ReactImageMagnify from "react-image-magnify";
 
-interface ImagesProps {
+interface images {
   images: string[];
 }
 
-export function ProductImages({ images }: ImagesProps) {
+export function ProductImages({ images }: images) {
   const [imageSelected, setImageSelected] = useState(0);
   const { imgpsition, setimgposition, imgColor, imgName, setImgName } =
     React.useContext(ShopContext);
 
-  const imagesWithColor = images.filter((image) => image.includes(imgColor));
+  const imagesWithcolor = images.filter((image) => image.includes(imgColor));
   const otherImages = images.filter((image) => !image.includes(imgColor));
-  const orderedImages = [...imagesWithColor, ...otherImages];
+  const orderedImages = [...imagesWithcolor, ...otherImages];
 
   const handleSelect = useCallback(
     (index: number) => {
@@ -37,43 +36,32 @@ export function ProductImages({ images }: ImagesProps) {
   }, [imgpsition, handleSelect]);
 
   return (
-    <Carousel className="w-full max-w-full rounded-3xl pb-1 bg-primary-color-100 overflow-hidden">
-      <CarouselContent>
-        {orderedImages.map((image, index) => (
+    <Carousel className="w-full max-w-full rounded-3xl pb-1 bg-primary-color-100 overflow-hidden ">
+      <CarouselContent className="">
+        {images.map((image, index) => (
           <CarouselItem key={index}>
-            <Card>
-              <CardContent className="flex flex-col aspect-square items-center justify-center p-0">
-                <ReactImageMagnify
-                  {...{
-                    smallImage: {
-                      alt: `Product Image ${index + 1}`,
-                      isFluidWidth: true,
-                      src: image,
-                    },
-                    largeImage: {
-                      src: image,
-                      width: 1200,
-                      height: 1200,
-                    },
-                    enlargedImageContainerStyle: { zIndex: 9 },
-                    lensStyle: { backgroundColor: "rgba(0,0,0,.3)" },
-                  }}
-                />
-              </CardContent>
-            </Card>
+            <div className="">
+              <Card>
+                <CardContent className="flex flex-col aspect-square items-center justify-center p-0 ">
+                  <img src={image} alt="" className="w-full mb-2" />
+                </CardContent>
+              </Card>
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
       <ul className="flex w-full justify-center items-center mt-1">
-        {orderedImages.map((_, ind) => (
+        {images.map((item, ind) => (
           <li
             key={ind}
             className={`w-2 h-2 mx-0.5 bg-[#d1cfc2] rounded-full ${
               ind === imgpsition ? "h-2.5 w-2.5 bg-grey" : "none"
             }`}
-          ></li>
+          >
+            {" "}
+          </li>
         ))}
       </ul>
     </Carousel>
